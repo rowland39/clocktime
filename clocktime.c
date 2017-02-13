a415fffe6f95d1f68a69625eebe8da8fb8b02e2c
Shaun Rowland <rowland@corelight.com>
2017-02-12 23:30:19 -0500

    Made output clearer and tested time(NULL) as well.

--- clocktime.c
+++ clocktime.c
@@ -5,16 +5,27 @@
 int
 main(void)
 {
+    time_t tsec;
     struct timespec ts;
+
+    if ((tsec = time(NULL)) == ((time_t) -1)) {
+        perror("time error");
+        exit(EXIT_FAILURE);
+    }
 
     if (clock_gettime(CLOCK_REALTIME, &ts) != 0) {
         perror("clock_gettime error");
         exit(EXIT_FAILURE);
     }
 
-    printf("ts.tv_sec       : %ld\n", ts.tv_sec);
-    printf("ts.tv_nsec      : %ld\n", ts.tv_nsec);
-    printf("ts.tv_nsec/1e9  : %f\n", ts.tv_nsec/1e9);
-    printf("Time Since Epoch: %f\n", ts.tv_sec + ts.tv_nsec/1e9);
+    printf("\nTime Test\n");
+    printf("---------\n");
+    printf("tsec = time(NULL)\n");
+    printf("clock_gettime(CLOCK_REALTIME, &ts)\n\n");
+    printf("tsec             = %ld s\n", tsec);
+    printf("ts.tv_sec        = %ld s\n", ts.tv_sec);
+    printf("ts.tv_nsec       = %ld ns\n", ts.tv_nsec);
+    printf("ts.tv_nsec/1e9   = %f s\n", ts.tv_nsec/1e9);
+    printf("Time Since Epoch = %f s\n\n", ts.tv_sec + ts.tv_nsec/1e9);
     exit(EXIT_SUCCESS);
 }
